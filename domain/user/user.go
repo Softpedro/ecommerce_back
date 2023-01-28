8b8327c4d6ffeb73f800a44aaa58a48ed2de6eb6
pedro.mollehuanca <pedromollehuanca@gmail.com>
2023-01-28 15:48:41 -0500

    package user

---
+++ domain/user/user.go
@@ -0,0 +1,15 @@
+package user
+
+import "github.com/Softpedro/ecommerce_back/model"
+
+type UseCase interface {
+	Create(m *model.User) error
+	GetByEmail(email string) (model.User, error)
+	GetAll() (model.Users, error)
+}
+
+type Storage interface {
+	Create(m *model.User) error
+	GetByEmail(email string) (model.User, error)
+	GetAll() (model.User, error)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Softpedro/ecommerce_back
+
+go 1.19
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+)
